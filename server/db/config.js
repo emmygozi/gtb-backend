2589f941b18c5d277595d903cb0ce1d55398505d
emmygozi <mygitmail@yahoo.com>
2019-12-10 19:37:59 +0100

    Add final touches to backend 4

--- server/db/config.js
+++ server/db/config.js
@@ -12,6 +12,6 @@ const myDatabaseConfig = {
 };
 
 // optimize if else code with tenary shortcut
-const pool = new Pool(process.env.DATABASE_URL);
+const pool = 'postgres://vcdzyehokpoxoh:2629c272309b311fe103e2a3b10f15ba8c22f7c5f2f336314c69445adc35719b@ec2-174-129-254-223.compute-1.amazonaws.com:5432/d1rne15fmjc0fp';
 
 export default pool;
